73bfc0ad3c8569af8ede8cf79add992130adcbb0
qeemoo <gyooheon2@gamil.com>
2025-04-18 22:54:42 +0900

    ✅ Add: Add logo link, score update, reset button
    로고 메인페이지로 링크연결, 스코어 0점이면 업데이트, 리셋 버튼으로 배열 재생성 기능 추가

--- src/app/apple/page.tsx
+++ src/app/apple/page.tsx
@@ -2,19 +2,26 @@
 
 import { useEffect, useState } from 'react';
 import S from './apple.module.css';
+import Link from 'next/link';
 
 const generateAppleGrid = () => {
   return Array.from(
     { length: 10 * 20 },
-    () => Math.floor(Math.random() * 9) + 1
+    () => Math.floor(Math.random() * 9) + 0 // test를 위하여 0으로 변경 1로 수정필요
   );
+};
+
+const findZeroPoint = (searchGrid: number[]) => {
+  return searchGrid.filter((n) => n === 0).length;
 };
 
 const Apple = () => {
   const [appleGrid, setAppleGrid] = useState<number[]>([]);
+  const [score, setScore] = useState<number>(0);
 
   useEffect(() => {
     setAppleGrid(generateAppleGrid());
+    setScore(findZeroPoint(generateAppleGrid()));
   }, []);
 
   return (
@@ -28,9 +35,21 @@ const Apple = () => {
           </div>
         </div>
         <div className={S.option_container}>
-          <div className={S.logo_box}>logo</div>
-          <div className={S.score_box}>score</div>
-          <div className={S.reset_box}>reset</div>
+          <div className={S.logo_box}>
+            <Link href='/'> logo</Link>
+          </div>
+          <div className={S.score_box}>{score}</div>
+          <div className={S.reset_box}>
+            <button
+              onClick={() => {
+                const newGrid = generateAppleGrid();
+                setAppleGrid(newGrid);
+                setScore(findZeroPoint(newGrid));
+              }}
+            >
+              reset
+            </button>
+          </div>
           <div className={S.volume_box}>vol</div>
         </div>
       </div>
