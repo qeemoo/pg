db20a84e7177bd615093c5f68696474b56fe20b2
qeemoo <gyooheon2@gamil.com>
2025-04-18 22:38:50 +0900

    ✅ Add: Add a random number grid
    메인 화면 랜덤숫자 그리드 추가

--- src/app/apple/page.tsx
+++ src/app/apple/page.tsx
@@ -1,10 +1,32 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import S from './apple.module.css';
 
+const generateAppleGrid = () => {
+  return Array.from(
+    { length: 10 * 20 },
+    () => Math.floor(Math.random() * 9) + 1
+  );
+};
+
 const Apple = () => {
+  const [appleGrid, setAppleGrid] = useState<number[]>([]);
+
+  useEffect(() => {
+    setAppleGrid(generateAppleGrid());
+  }, []);
+
   return (
     <div className={S.container}>
       <div className={S.play_container}>
-        <div className={S.main_container}>main</div>
+        <div className={S.main_container}>
+          <div className={S.main_grid}>
+            {appleGrid.map((number, index) => (
+              <div key={index}>{number}</div>
+            ))}
+          </div>
+        </div>
         <div className={S.option_container}>
           <div className={S.logo_box}>logo</div>
           <div className={S.score_box}>score</div>
